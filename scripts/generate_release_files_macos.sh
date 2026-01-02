bce2132fc5e53aa51f205d1e45c32609bcc31256
James Bruska <bruskatech@protonmail.com>
2026-01-02 00:38:28 -0300

    Changes workflow for rust-build version and fixed generate_release_files_macos.sh

--- scripts/generate_release_files_macos.sh
+++ scripts/generate_release_files_macos.sh
@@ -4,11 +4,15 @@ set -euo pipefail
 
 cargo build -r
 
-PACKAGE_VERSION=$(grep '^version =' ../Cargo.toml | head -n1 | cut -d '"' -f2)
-PACKAGE_VERSION="v${PACKAGE_VERSION}"
-OUTPUT_NAME="conda-share_${PACKAGE_VERSION}_aarch64-apple-darwin"
+PACKAGE_VERSION=$(grep '^version =' ../conda-share-cli/Cargo.toml | head -n1 | cut -d '"' -f2)
+OUTPUT_NAME_CLI="conda-share_v${PACKAGE_VERSION}_aarch64-apple-darwin"
+PACKAGE_VERSION=$(grep '^version =' ../conda-share-gui/Cargo.toml | head -n1 | cut -d '"' -f2)
+OUTPUT_NAME_GUI="conda-share-gui_v${PACKAGE_VERSION}_aarch64-apple-darwin"
 
 mkdir -p ../release_files
 
-ditto -c -k --sequesterRsrc ../target/release/conda-share "../release_files/${OUTPUT_NAME}.zip"
-shasum -a 256 "../release_files/${OUTPUT_NAME}.zip" > "../release_files/${OUTPUT_NAME}.sha256"
+ditto -c -k --sequesterRsrc ../target/release/conda-share "../release_files/${OUTPUT_NAME_CLI}.zip"
+shasum -a 256 "../release_files/${OUTPUT_NAME_CLI}.zip" > "../release_files/${OUTPUT_NAME_CLI}.sha256"
+
+ditto -c -k --sequesterRsrc ../target/release/conda-share-gui "../release_files/${OUTPUT_NAME_GUI}.zip"
+shasum -a 256 "../release_files/${OUTPUT_NAME_GUI}.zip" > "../release_files/${OUTPUT_NAME_GUI}.sha256"
